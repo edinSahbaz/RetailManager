dde1042e47f0d147c719343fe6e7e007392c298d
edin.sahbaz <edin.sahbaz@hotmail.com>
2019-06-10 17:34:48 +0200

    Code cleanup
    Refactored code and removed unnecessary
    usings.

--- RMDataManager.Library/DataAccess/ProductData.cs
+++ RMDataManager.Library/DataAccess/ProductData.cs
@@ -1,10 +1,6 @@
 ﻿using RMDataManager.Library.Internal.DataAccess;
 using RMDataManager.Library.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RMDataManager.Library.DataAccess
 {
--- RMDataManager.Library/Models/ProductModel.cs
+++ RMDataManager.Library/Models/ProductModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RMDataManager.Library.Models
+﻿namespace RMDataManager.Library.Models
 {
     public class ProductModel
     {
