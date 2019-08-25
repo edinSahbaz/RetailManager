a83a7d9000c535ef4b08c97216179e639fd71ca9
edin.sahbaz <edin.sahbaz@hotmail.com>
2019-08-25 15:36:46 +0200

    Fix Identity Scope
    Used Scope_Identity() now

--- RMData/dbo/Stored Procedures/spSale_Insert.sql
+++ RMData/dbo/Stored Procedures/spSale_Insert.sql
@@ -13,5 +13,5 @@ BEGIN
 	INSERT INTO dbo.Sale(CashierId, SaleDate, SubTotal, Tax, Total)
 	VALUES(@CashierId, @SaleDate, @SubTotal, @Tax, @Total);
 
-	SELECT @Id = @@IDENTITY;
+	SELECT @Id = Scope_IDENTITY();
 END
